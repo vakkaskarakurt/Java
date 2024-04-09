35f623bb7e1e3da346278bf1c7b48c73424ef07a
vakkaskarakurt <karakurtvakkas@gmail.com>
2024-04-10 00:20:08 +0300

    First

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        System.out.println("JAVA IS BEST");
+        System.out.println("PROGRAM LANGUAGE");
+
+    
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 3;
+        int c = 4;
+
+        int sum = a + b + c;
+
+        System.out.println("Sum: " + sum);
+
+    }
+}
