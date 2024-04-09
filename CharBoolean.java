88049c67e3a5f94628a6cdc1abdca03d4408c48d
vakkaskarakurt <karakurtvakkas@gmail.com>
2024-04-10 02:52:14 +0300

    .

---
+++ CharBoolean.java
@@ -0,0 +1,21 @@
+public class CharBoolean {
+    public static void main(String[] args) {
+        // Char Data Type: Using for single character. 2 byte - 16 bit takes memory. 2^16  different characters.
+        // Boolean Data Type: Using for logical operations. 1 bit takes memory. True or False.
+
+        char a = 'A';
+        char b = '?';
+        char c = 1000;
+        char d = '\u0152';  // Unicode Character
+        System.out.println(d);
+
+
+        boolean m = true;
+        boolean n = false;
+
+        System.out.println(m);
+        System.out.println(n);
+
+    }
+    
+}
